be0d5033377654661fb8b1506a5712be264b04d3
Viacheslav Andreev <maxivimax@icloud.com>
2022-04-16 23:57:09 +0300

    ...

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
 if (process.argv.length !== 4) {
   console.error("./server.js <auth token> <webserver base URL>");
   process.exit(1);
-} 
+}
+
 
 var url = require("url");
 var botgram = require("botgram");
 var express = require("express");
 
-var gameName = "waitsimulator";
-var publicBase = process.argv[3];
 var bot = botgram(process.argv[2]);
 var server = express();
 
+var gameName = "waitsimulator";
+var publicBase = process.argv[3];
 var queries = {};
+
 
 bot.command("help", function (msg, reply, next) {
   reply.text("/game для получения игры)");
@@ -28,14 +30,17 @@ bot.callback(function (query, next) {
   if (query.gameShortName !== gameName) return next();
   queries[query.id] = query;
   query.answer({
-    url: url.resolve(publicBase, "/telegramBot/game.html?id="+query.id)
+    url: url.resolve(publicBase, "/telegramBot/game.html?id=" + query.id)
   });
 });
+
+console.log(__dirname)
+server.use(express.static(__dirname + '/assets'));
 
 server.get("/telegramBot/game.html", function (req, res, next) {
   if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
   console.log("Отправка игры на %s...", queries[req.query.id].from.name);
-  res.sendFile(__dirname + "/game.html");
+  res.sendFile(__dirname + "/assets/game.html");
 });
 
 server.get("/telegramBot/submitScore/:score", function (req, res, next) {
@@ -46,7 +51,7 @@ server.get("/telegramBot/submitScore/:score", function (req, res, next) {
   bot.setGameScore(query.from, parseInt(req.params.score), {
     chat: query.message ? query.message.chat : null,
     message: query.message || query.inlineMessageId,
-  }, function (err, result) {});
+  }, function (err, result) { });
 });
 
 server.get("/telegramBot/test.html", function (req, res, next) {
